8048a16b86be26d98996b62ee5a90da68c4508fa
banira4649 <banira.game@outlook.jp>
2024-05-26 17:27:01 +0900

    EventListenerを追加

--- src/Main.php
+++ src/Main.php
@@ -4,25 +4,19 @@ declare(strict_types=1);
 
 namespace banira4649\Sumo;
 
-use pocketmine\event\entity\EntityDamageEvent;
-use pocketmine\event\entity\EntityTeleportEvent;
-use pocketmine\event\Listener;
-use pocketmine\event\player\PlayerExhaustEvent;
-use pocketmine\event\player\PlayerJoinEvent;
-use pocketmine\event\player\PlayerQuitEvent;
+use banira4649\Sumo\event\EventListener;
 use pocketmine\plugin\PluginBase;
-use pocketmine\scheduler\ClosureTask;
 use pocketmine\player\{Player, GameMode};
 use pocketmine\math\Vector3;
 use pocketmine\Server;
 use pocketmine\utils\Config;
 use pocketmine\world\World;
 use pocketmine\world\WorldManager;
-use banira4649\Sumo\commands\{SumoCommand, EntryCommand, EntrylistCommand, SumoarenaCommand};
+use banira4649\Sumo\command\{SumoCommand, EntryCommand, EntrylistCommand, SumoarenaCommand};
 use banira4649\Sumo\game\Game;
 use Symfony\Component\Filesystem\Path;
 
-class Main extends PluginBase implements Listener{
+class Main extends PluginBase{
     public const WORLD_NAME = "sumo";
 
     public WorldManager $worldManager;
@@ -33,7 +27,7 @@ class Main extends PluginBase implements Listener{
     public ?World $stage;
 
     public function onEnable(): void{
-        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->getServer()->getPluginManager()->registerEvents(new EventListener($this), $this);
         $this->getServer()->getCommandMap()->registerAll($this->getName(), [
             new SumoCommand("sumo", $this),
             new EntryCommand("entry", $this),
@@ -64,66 +58,6 @@ class Main extends PluginBase implements Listener{
             $stageData->get("pos2")[2]
         );
         $this->game = new Game($this);
-    }
-
-    public function playerJoinEvent(PlayerJoinEvent $event): void{
-        $player = $event->getPlayer();
-        foreach($this->game->getPlayers() as $players){
-            if($players->getName() === $player->getName()){
-                $this->game->removePlayer($players);
-                $this->game->addPlayer($player);
-            }
-        }
-    }
-
-    public function playerQuitEvent(PlayerQuitEvent $event): void{
-        $player = $event->getPlayer();
-        if($this->game->isCombat($player)){
-            $this->game->win($this->game->getEnemy($player));
-        }
-    }
-
-    public function playerExhaustEvent(PlayerExhaustEvent $event): void{
-        if($event->getPlayer()->getWorld() === $this->stage){
-            $event->cancel();
-        }
-    }
-
-    public function entityDamageEvent(EntityDamageEvent $event): void{
-        $entity = $event->getEntity();
-        if($entity instanceof Player){
-            if($entity->getWorld() === $this->stage){
-                $this->getScheduler()->scheduleDelayedTask(
-                    new ClosureTask(function() use ($entity): void{
-                        $entity->setHealth($entity->getMaxHealth());
-                    }),
-                1);
-                if($event->getCause() === EntityDamageEvent::CAUSE_VOID){
-                    $event->cancel();
-                    if($this->game->isCombat($entity)){
-                        $this->game->win($this->game->getEnemy($entity));
-                    }else{
-                        $entity->teleport($this->sumoPos0);
-                    }
-                }
-                if(!$this->game->isCombat($entity)){
-                    $event->cancel();
-                }elseif($event->getCause() !== EntityDamageEvent::CAUSE_ENTITY_ATTACK){
-                    $event->cancel();
-                }
-            }
-        }
-    }
-
-    public function entityTeleportEvent(EntityTeleportEvent $event): void{
-        $entity = $event->getEntity();
-        if($entity instanceof Player){
-            if($this->game->isCombat($entity)){
-                if($event->getFrom()->getWorld() !== $event->getFrom()->getWorld()){
-                    $this->game->win($this->game->getEnemy($entity));
-                }
-            }
-        }
     }
 
     public static function resetPlayer(Player $player): void{
---
+++ src/event/EventListener.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+namespace banira4649\Sumo\event;
+
+use banira4649\Sumo\Main;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityTeleportEvent;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerExhaustEvent;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\player\Player;
+use pocketmine\scheduler\ClosureTask;
+
+class EventListener implements Listener{
+
+    private Main $main;
+
+    public function __construct(Main $main){
+        $this->main = $main;
+    }
+
+    public function playerJoinEvent(PlayerJoinEvent $event): void{
+        $player = $event->getPlayer();
+        foreach($this->main->game->getPlayers() as $players){
+            if($players->getName() === $player->getName()){
+                $this->main->game->removePlayer($players);
+                $this->main->game->addPlayer($player);
+            }
+        }
+    }
+
+    public function playerQuitEvent(PlayerQuitEvent $event): void{
+        $player = $event->getPlayer();
+        if($this->main->game->isCombat($player)){
+            $this->main->game->win($this->main->game->getEnemy($player));
+        }
+    }
+
+    public function playerExhaustEvent(PlayerExhaustEvent $event): void{
+        if($event->getPlayer()->getWorld() === $this->main->stage){
+            $event->cancel();
+        }
+    }
+
+    public function entityDamageEvent(EntityDamageEvent $event): void{
+        $entity = $event->getEntity();
+        if($entity instanceof Player){
+            if($entity->getWorld() === $this->main->stage){
+                $this->main->getScheduler()->scheduleDelayedTask(
+                    new ClosureTask(function() use ($entity): void{
+                        $entity->setHealth($entity->getMaxHealth());
+                    }),
+                    1);
+                if($event->getCause() === EntityDamageEvent::CAUSE_VOID){
+                    $event->cancel();
+                    if($this->main->game->isCombat($entity)){
+                        $this->main->game->win($this->main->game->getEnemy($entity));
+                    }else{
+                        $entity->teleport($this->main->sumoPos0);
+                    }
+                }
+                if(!$this->main->game->isCombat($entity)){
+                    $event->cancel();
+                }elseif($event->getCause() !== EntityDamageEvent::CAUSE_ENTITY_ATTACK){
+                    $event->cancel();
+                }
+            }
+        }
+    }
+
+    public function entityTeleportEvent(EntityTeleportEvent $event): void{
+        $entity = $event->getEntity();
+        if($entity instanceof Player){
+            if($this->main->game->isCombat($entity)){
+                if($event->getFrom()->getWorld() !== $event->getFrom()->getWorld()){
+                    $this->main->game->win($this->main->game->getEnemy($entity));
+                }
+            }
+        }
+    }
+}
