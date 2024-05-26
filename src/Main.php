34f690d59bf4698b8efb344fc0c5fa16257ab7d5
banira4649 <banira.game@outlook.jp>
2024-05-26 16:58:40 +0900

    config.jsonにより座標の管理が可能に

--- src/Main.php
+++ src/Main.php
@@ -14,13 +14,15 @@ use pocketmine\plugin\PluginBase;
 use pocketmine\scheduler\ClosureTask;
 use pocketmine\player\{Player, GameMode};
 use pocketmine\math\Vector3;
+use pocketmine\Server;
+use pocketmine\utils\Config;
 use pocketmine\world\World;
 use pocketmine\world\WorldManager;
 use banira4649\Sumo\commands\{SumoCommand, EntryCommand, EntrylistCommand, SumoarenaCommand};
 use banira4649\Sumo\game\Game;
+use Symfony\Component\Filesystem\Path;
 
 class Main extends PluginBase implements Listener{
-
     public const WORLD_NAME = "sumo";
 
     public WorldManager $worldManager;
@@ -33,17 +35,34 @@ class Main extends PluginBase implements Listener{
     public function onEnable(): void{
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
         $this->getServer()->getCommandMap()->registerAll($this->getName(), [
-			new SumoCommand("sumo", $this),
+            new SumoCommand("sumo", $this),
             new EntryCommand("entry", $this),
             new EntrylistCommand("entrylist", $this),
-            new SumoarenaCommand("sumoarena", $this)
-		]);
+            new SumoarenaCommand("sumoarena", $this),
+        ]);
         $this->worldManager = $this->getServer()->getWorldManager();
         $this->worldManager->loadWorld(self::WORLD_NAME);
         $this->stage = $this->worldManager->getWorldByName(self::WORLD_NAME);
-        $this->sumoPos0 = new Vector3(274.5, 31.5, 255.5);
-        $this->sumoPos1 = new Vector3(255.5, 31.5, 259.5);
-        $this->sumoPos2 = new Vector3(255.5, 31.5, 251.5);
+        $stageData = new Config(
+            Path::join(Server::getInstance()->getDataPath(), "worlds", self::WORLD_NAME, 'config.json'),
+            Config::JSON,
+            ["spawn" => [0, 0, 0], "pos1" => [0, 0, 0], "pos2" => [0, 0, 0]]
+        );
+        $this->sumoPos0 = new Vector3(
+            $stageData->get("spawn")[0],
+            $stageData->get("spawn")[1],
+            $stageData->get("spawn")[2]
+        );
+        $this->sumoPos1 = new Vector3(
+            $stageData->get("pos1")[0],
+            $stageData->get("pos1")[1],
+            $stageData->get("pos1")[2]
+        );
+        $this->sumoPos2 = new Vector3(
+            $stageData->get("pos2")[0],
+            $stageData->get("pos2")[1],
+            $stageData->get("pos2")[2]
+        );
         $this->game = new Game($this);
     }
 
@@ -59,9 +78,9 @@ class Main extends PluginBase implements Listener{
 
     public function playerQuitEvent(PlayerQuitEvent $event): void{
         $player = $event->getPlayer();
-		if($this->game->isCombat($player)){
-			$this->game->win($this->game->getEnemy($player));
-		}
+        if($this->game->isCombat($player)){
+            $this->game->win($this->game->getEnemy($player));
+        }
     }
 
     public function playerExhaustEvent(PlayerExhaustEvent $event): void{
@@ -70,16 +89,15 @@ class Main extends PluginBase implements Listener{
         }
     }
 
-    public function entityDamageEvent(EntityDamageEvent $event): void
-    {
+    public function entityDamageEvent(EntityDamageEvent $event): void{
         $entity = $event->getEntity();
         if($entity instanceof Player){
             if($entity->getWorld() === $this->stage){
-                $this->getScheduler()->scheduleDelayedTask(new ClosureTask(
-        			function () use ($entity): void{
+                $this->getScheduler()->scheduleDelayedTask(
+                    new ClosureTask(function() use ($entity): void{
                         $entity->setHealth($entity->getMaxHealth());
-        			}
-        		), 1);
+                    }),
+                1);
                 if($event->getCause() === EntityDamageEvent::CAUSE_VOID){
                     $event->cancel();
                     if($this->game->isCombat($entity)){
@@ -119,5 +137,4 @@ class Main extends PluginBase implements Listener{
         $player->setOnFire(0);
         $player->setGameMode(GameMode::ADVENTURE());
     }
-
 }
